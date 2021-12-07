21b85ded7f1333336157d752856b8775690aaaa9
chun <34814629+wataruds@users.noreply.github.com>
2021-12-07 18:28:40 +0900

    少し更新

---
+++ src/ChildArea.jsx
@@ -0,0 +1,20 @@
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open } = props;
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+});
